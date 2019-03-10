e2b59614ee3550f42a038b78e359e936f27ea2ca
Michael Grieco <modularglitch@gmail.com>
2019-03-10 19:31:14 -0400

    Finished expandable image section

--- script/main.js
+++ script/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+    $("#navbar").load("/nav.html #main-nav");
+
+    $("span.expandable-zone").each(function() {
+        var img_src = $(this).attr("img-src");
+        var src_title = $(this).attr("src-title");
+
+        $(this).append("<div class=\"expandable-content\">" +
+                        "\n\t<div class=\"img-section\">" +
+                        "\n\t\t<img class=\"img-with-link\" src=\"" + img_src + "\" src-title=\"" + src_title + "\"/>" +
+                        "\n\t</div>" +
+                        "\n</div>");
+    });
+
     $(".img-section").each(function() {
         var src = $(this).find(".img-with-link").attr("src");
         var src_title = $(this).find(".img-with-link").attr("src-title");
 
         $(this).append("<p class=\"img-link\">Source: <a target=\"_blank\" href=\"" + src + "\">" + src_title + "</a></p>");
-
-        /* .append( "<p>Test</p>" ); 
-            <p class="img-link">Source: <a target="_blank" href=""></a></p>
-        */
-    
-        /*$(this).find(".img-link").find("a").attr("href", src);
-        $(this).find(".img-link").find("a").html(src_title);*/
     });
-
-    $("#navbar").load("/nav.html #main-nav");
 });
