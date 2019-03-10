44e9f597aa3ae0442ec9e57c96977d58c1a27450
Michael Grieco <modularglitch@gmail.com>
2019-03-10 14:37:54 -0400

    Added content to home screen

--- script/main.js
+++ script/main.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
     $(".img-section").each(function() {
         var src = $(this).find(".img-with-link").attr("src");
         var src_title = $(this).find(".img-with-link").attr("src-title");
+
+        $(this).append("<p class=\"img-link\">Source: <a target=\"_blank\" href=\"" + src + "\">" + src_title + "</a></p>");
+
+        /* .append( "<p>Test</p>" ); 
+            <p class="img-link">Source: <a target="_blank" href=""></a></p>
+        */
     
-        $(this).find(".img-link").find("a").attr("href", src);
-        $(this).find(".img-link").find("a").html(src_title);
+        /*$(this).find(".img-link").find("a").attr("href", src);
+        $(this).find(".img-link").find("a").html(src_title);*/
     });
 
     $("#navbar").load("/nav.html #main-nav");
-/*
-    $.get("/nav.html", function(nav_html) {
-        $("#navbar").html(nav_html);
-    });*/
 });
