77729be9f6804a2ea9adc036404f5f293d589614
Michael Grieco <modularglitch@gmail.com>
2019-03-11 23:38:10 -0400

    Changed jquery identifier

--- script/main.js
+++ script/main.js
@@ -1,38 +1,40 @@
-$(document).ready(function() {
-    $("#navbar").load("/architectureDeQuebec/nav.html #main-nav");
+jQuery(document).ready(function() {
+    jQuery.noConflict();
 
-    $("span.expandable-zone").each(function() {
-        var img_src = $(this).attr("img-src");
-        var src_title = $(this).attr("src-title");
+    jQuery("#navbar").load("/architectureDeQuebec/nav.html #main-nav");
 
-        $(this).append("<div class=\"expandable-content\">" +
+    jQuery("span.expandable-zone").each(function() {
+        var img_src = jQuery(this).attr("img-src");
+        var src_title = jQuery(this).attr("src-title");
+
+        jQuery(this).append("<div class=\"expandable-content\">" +
                         "\n\t<div class=\"img-section\">" +
                         "\n\t\t<img class=\"img-with-link\" src=\"" + img_src + "\" src-title=\"" + src_title + "\"/>" +
                         "\n\t</div>" +
                         "\n</div>");
     });
 
-    $(".img-section").each(function() {
-        var src = $(this).find(".img-with-link").attr("src");
-        var src_title = $(this).find(".img-with-link").attr("src-title");
+    jQuery(".img-section").each(function() {
+        var src = jQuery(this).find(".img-with-link").attr("src");
+        var src_title = jQuery(this).find(".img-with-link").attr("src-title");
 
-        $(this).append("<p class=\"img-link\">Source: <a target=\"_blank\" href=\"" + src + "\">" + src_title + "</a></p>");
+        jQuery(this).append("<p class=\"img-link\">Source: <a target=\"_blank\" href=\"" + src + "\">" + src_title + "</a></p>");
     });
 
     var num = 1;
 
-    $("footnote").each(function() {
-        var href = $(this).html();
+    jQuery("footnote").each(function() {
+        var href = jQuery(this).html();
 
-        $(this).replaceWith("<sup><a target=\"_blank\" href=\"" + href + "\">" + num + "</a></sup>");
+        jQuery(this).replaceWith("<sup><a target=\"_blank\" href=\"" + href + "\">" + num + "</a></sup>");
 
         num++;
     });
 
-    $("definition").each(function() {
-        var original = $(this).html();
-        var definition = $(this).attr("def");
+    jQuery("definition").each(function() {
+        var original = jQuery(this).html();
+        var definition = jQuery(this).attr("def");
 
-        $(this).replaceWith("<span tooltip=\"true\" tooltip-position=\"top\" tooltip-data=\"'" + definition + "'\">" + original + "</span>");
+        jQuery(this).replaceWith("<span tooltip=\"true\" tooltip-position=\"top\" tooltip-data=\"'" + definition + "'\">" + original + "</span>");
     });
 });
