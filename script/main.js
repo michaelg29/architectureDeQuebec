198941c2a84c5cbefa20e3a0715f42e3e6141510
michaelg29 <michaelgriecocoding@gmail.com>
2020-06-16 14:27:46 -0400

    Updated links

--- script/main.js
+++ script/main.js
@@ -1,20 +1,20 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     jQuery.noConflict();
 
     jQuery("#navbar").load("/architectureDeQuebec/nav.html #main-nav");
 
-    jQuery("span.expandable-zone").each(function() {
+    jQuery("span.expandable-zone").each(function () {
         var img_src = jQuery(this).attr("img-src");
         var src_title = jQuery(this).attr("src-title");
 
         jQuery(this).append("<div class=\"expandable-content\">" +
-                        "\n\t<div class=\"img-section\">" +
-                        "\n\t\t<img class=\"img-with-link\" src=\"" + img_src + "\" src-title=\"" + src_title + "\"/>" +
-                        "\n\t</div>" +
-                        "\n</div>");
+            "\n\t<div class=\"img-section\">" +
+            "\n\t\t<img class=\"img-with-link\" src=\"" + img_src + "\" src-title=\"" + src_title + "\"/>" +
+            "\n\t</div>" +
+            "\n</div>");
     });
 
-    jQuery(".img-section").each(function() {
+    jQuery(".img-section").each(function () {
         var src = jQuery(this).find(".img-with-link").attr("src");
         var src_title = jQuery(this).find(".img-with-link").attr("src-title");
 
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
 
     var num = 1;
 
-    jQuery("footnote").each(function() {
+    jQuery("footnote").each(function () {
         var href = jQuery(this).html();
 
         jQuery(this).replaceWith("<sup><a target=\"_blank\" href=\"" + href + "\">" + num + "</a></sup>");
@@ -31,7 +31,7 @@ jQuery(document).ready(function() {
         num++;
     });
 
-    jQuery("definition").each(function() {
+    jQuery("definition").each(function () {
         var original = jQuery(this).html();
         var definition = jQuery(this).attr("def");
 
